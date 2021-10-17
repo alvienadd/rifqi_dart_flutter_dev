cc8a6cbf1fb7a5ce65870a9ad8967904086b7ec2
Alvien Laksono <alvien.add@gmail.com>
2021-10-17 09:56:16 +0700

    Revisi UI - Remove Padding bagian ListView, Edit SizedBox Bagian Atas dan SizedBox Bagian Bawah

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,142 +14,137 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         bottom: false,
-        child: Padding(
-            padding: EdgeInsets.symmetric(vertical: edge),
-            child: ListView(
-              children: [
-                //Note: Title/Header
-                Padding(
-                    padding: EdgeInsets.only(left: 24),
-                    child: Text(
-                      'Explore Now',
-                      style: blackTextStyle.copyWith(fontSize: 24),
-                    )),
-                SizedBox(
-                  height: 2,
-                ),
-                Padding(
-                    padding: EdgeInsets.only(left: 24),
-                    child: Text(
-                      'Mencari kosan yang cozy',
-                      style: greyTextStyle.copyWith(fontSize: 16),
-                    )),
-                SizedBox(height: 30),
-                //Note: Popular Cities
-                Padding(
-                    padding: EdgeInsets.only(left: 24),
-                    child: Text(
-                      'Popular Cities',
-                      style: regularTextStyle.copyWith(fontSize: 16),
-                    )),
-                SizedBox(height: 16),
-                Container(
-                  height: 150,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      SizedBox(width: 24),
-                      CityCard(City(
-                          id: 1,
-                          name: "Jakarta",
-                          imageUrl: 'assets/city1.png')),
-                      SizedBox(width: 20),
-                      CityCard(City(
-                          id: 2,
-                          name: "Bandung",
-                          imageUrl: 'assets/city2.png',
-                          isPopular: true)),
-                      SizedBox(width: 20),
-                      CityCard(City(
-                          id: 3,
-                          name: "Surabaya",
-                          imageUrl: 'assets/city3.png')),
-                      SizedBox(width: 24),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                //Note: Recomended Space
-                Padding(
-                    padding: EdgeInsets.only(left: 24),
-                    child: Text(
-                      'Recommended Space',
-                      style: regularTextStyle.copyWith(fontSize: 16),
-                    )),
-                SizedBox(
-                  height: 16,
-                ),
+        child: ListView(
+          children: [
+            SizedBox(height: edge),
+            //Note: Title/Header
+            Padding(
+                padding: EdgeInsets.only(left: 24),
+                child: Text(
+                  'Explore Now',
+                  style: blackTextStyle.copyWith(fontSize: 24),
+                )),
+            SizedBox(
+              height: 2,
+            ),
+            Padding(
+                padding: EdgeInsets.only(left: 24),
+                child: Text(
+                  'Mencari kosan yang cozy',
+                  style: greyTextStyle.copyWith(fontSize: 16),
+                )),
+            SizedBox(height: 30),
+            //Note: Popular Cities
+            Padding(
+                padding: EdgeInsets.only(left: 24),
+                child: Text(
+                  'Popular Cities',
+                  style: regularTextStyle.copyWith(fontSize: 16),
+                )),
+            SizedBox(height: 16),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 24),
+                  CityCard(City(
+                      id: 1, name: "Jakarta", imageUrl: 'assets/city1.png')),
+                  SizedBox(width: 20),
+                  CityCard(City(
+                      id: 2,
+                      name: "Bandung",
+                      imageUrl: 'assets/city2.png',
+                      isPopular: true)),
+                  SizedBox(width: 20),
+                  CityCard(City(
+                      id: 3, name: "Surabaya", imageUrl: 'assets/city3.png')),
+                  SizedBox(width: 24),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            //Note: Recomended Space
+            Padding(
+                padding: EdgeInsets.only(left: 24),
+                child: Text(
+                  'Recommended Space',
+                  style: regularTextStyle.copyWith(fontSize: 16),
+                )),
+            SizedBox(
+              height: 16,
+            ),
 
-                Padding(
-                    padding: EdgeInsets.symmetric(horizontal: edge),
-                    child: Column(
-                      children: [
-                        SpaceCard(
-                          Space(
-                              id: 1,
-                              name: 'Kuretakeso Hott',
-                              imageUrl: 'assets/space1.png',
-                              price: 52,
-                              city: 'Bandung',
-                              country: 'Germany',
-                              rating: 4),
-                        ),
-                        SizedBox(height: 30),
-                        SpaceCard(
-                          Space(
-                              id: 2,
-                              name: 'Roemah Nenek',
-                              imageUrl: 'assets/space2.png',
-                              price: 11,
-                              city: 'Seattle',
-                              country: 'Bogor',
-                              rating: 5),
-                        ),
-                        SizedBox(height: 30),
-                        SpaceCard(
-                          Space(
-                              id: 3,
-                              name: 'Darrling How',
-                              imageUrl: 'assets/space3.png',
-                              price: 20,
-                              city: 'Jakarta',
-                              country: 'Indonesia',
-                              rating: 3),
-                        )
-                      ],
-                    )),
-                SizedBox(height: 30),
-                //NOTE:Tips & Guidance
-                Padding(
-                    padding: EdgeInsets.only(left: 24),
-                    child: Text(
-                      'Tips & Guidance',
-                      style: regularTextStyle.copyWith(fontSize: 16),
-                    )),
-                SizedBox(height: 16),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: edge),
-                  child: Column(
-                    children: [
-                      TipsCard(Tips(
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: edge),
+                child: Column(
+                  children: [
+                    SpaceCard(
+                      Space(
                           id: 1,
-                          title: 'City Guidelines',
-                          imageUrl: "assets/tips1.png",
-                          updatedAt: "20 Apr")),
-                      SizedBox(height: 20),
-                      TipsCard(Tips(
+                          name: 'Kuretakeso Hott',
+                          imageUrl: 'assets/space1.png',
+                          price: 52,
+                          city: 'Bandung',
+                          country: 'Germany',
+                          rating: 4),
+                    ),
+                    SizedBox(height: 30),
+                    SpaceCard(
+                      Space(
                           id: 2,
-                          title: 'Jakarta Fairship',
-                          imageUrl: "assets/tips2.png",
-                          updatedAt: "11 Dec"))
-                    ],
-                  ),
-                ),
-                SizedBox(height: 50),
-              ],
-            )),
+                          name: 'Roemah Nenek',
+                          imageUrl: 'assets/space2.png',
+                          price: 11,
+                          city: 'Seattle',
+                          country: 'Bogor',
+                          rating: 5),
+                    ),
+                    SizedBox(height: 30),
+                    SpaceCard(
+                      Space(
+                          id: 3,
+                          name: 'Darrling How',
+                          imageUrl: 'assets/space3.png',
+                          price: 20,
+                          city: 'Jakarta',
+                          country: 'Indonesia',
+                          rating: 3),
+                    )
+                  ],
+                )),
+            SizedBox(height: 30),
+            //NOTE:Tips & Guidance
+            Padding(
+                padding: EdgeInsets.only(left: 24),
+                child: Text(
+                  'Tips & Guidance',
+                  style: regularTextStyle.copyWith(fontSize: 16),
+                )),
+            SizedBox(height: 16),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Column(
+                children: [
+                  TipsCard(Tips(
+                      id: 1,
+                      title: 'City Guidelines',
+                      imageUrl: "assets/tips1.png",
+                      updatedAt: "20 Apr")),
+                  SizedBox(height: 20),
+                  TipsCard(Tips(
+                      id: 2,
+                      title: 'Jakarta Fairship',
+                      imageUrl: "assets/tips2.png",
+                      updatedAt: "11 Dec"))
+                ],
+              ),
+            ),
+            SizedBox(height: 50 + edge),
+          ],
+        ),
       ),
       floatingActionButton: Container(
         height: 65,
